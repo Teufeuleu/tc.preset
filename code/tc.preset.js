af803d109eb4ccc1f28a5e7ab90d7dda4fd84ea2
Théophile Clet <contact@tflcl.xyz>
2024-11-29 16:58:23 +0100

    autowatch

--- code/tc.preset.js
+++ code/tc.preset.js
@@ -19,7 +19,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/gpl-3.0.txt>.
 */
 
-autowatch = 1;
+autowatch = 0;
 // When developping, autowatch = 1 isn't enough. You also need to manually call the loadbang function, and then re-binding the pattrstorage.
 // A "loadbang, pattrstorage test" message does the trick.
 
